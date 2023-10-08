521e7ffffdfc4bb3f1bfa05d467dd2f6b602716c
Rolex <manrrolo@gmail.com>
2023-10-08 13:50:09 +0100

    StackArrayList comments and Documentation Using JavaDoc Added folder just for the StackArrayList Documentation

--- src/pt/pa/adts/StackArrayList.java
+++ src/pt/pa/adts/StackArrayList.java
@@ -2,19 +2,44 @@ package pt.pa.adts;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+/**
+ * Isto é uma Classe "StackArrayList" herdada da interface "Stack".
+ *
+ * @param <T> tipo de armazenamento genérico
+ *
+ * @version %I% %G%
+ * @author Miguel Rôlo
+ */
 public class StackArrayList<T> implements Stack<T> {
 
+    /**
+     * Declaração de uma variável estática do tipo inteiro, define a capacidade máxima da Stack.
+     */
     private static final int DEFAULT_CAPACITY = 40;
 
+    /**
+     * Array do tipo genérico, que armazena todos os elementos da Stack.
+     */
     private T[] elements;
+    /**
+     * Variável do tipo inteiro que representa o tamanho da nossa Stack.
+     */
     private int size;
 
+    /**
+     * Criação do array Stack, inicia vazio porém com capacidade 40;
+     */
     @SuppressWarnings("unchecked")
     public StackArrayList() {
         this.elements = (T[])new Object[DEFAULT_CAPACITY];
         this.size = 0;
     }
 
+    /**
+     * Adiciona elementos à Stack.
+     * @param element             Tipo de armazenamento genérico
+     * @throws FullStackException Devolve uma string quando a Stack estiver cheia.
+     */
     @Override
     public void push(T element) throws FullStackException {
         if(this.size >= this.elements.length) throw new FullStackException();
@@ -22,6 +47,10 @@ public class StackArrayList<T> implements Stack<T> {
         this.elements[this.size++] = element;
     }
 
+    /**
+     * @return                     Remove e devolve o elemento no topo da Stack.
+     * @throws EmptyStackException Devolve uma string quando a Stack estiver vazia.
+     */
     @Override
     public T pop() throws EmptyStackException {
         if(this.size == 0) throw new EmptyStackException();
@@ -33,21 +62,34 @@ public class StackArrayList<T> implements Stack<T> {
         return elem;
     }
 
+    /**
+     * @return                     Devolve, sem remover, o elemento no topo da Stack.
+     * @throws EmptyStackException Devolve uma string quando a Stack estiver vazia.
+     */
     @Override
     public T peek() throws EmptyStackException {
         throw new NotImplementedException();
     }
 
+    /**
+     * @return Devolve o tamanho (Nrº de Elementos) atual da Stack.
+     */
     @Override
     public int size() {
         return this.size;
     }
 
+    /**
+     * @return Devolve um valor lógico (true/false) se a Stack for vazia ou não.
+     */
     @Override
     public boolean isEmpty() {
         throw new NotImplementedException();
     }
 
+    /**
+     * Devolve um valor lógico (true/false) que indica se a Stack está vazia ou não.
+     */
     @Override
     public void clear() {
         throw new NotImplementedException();
